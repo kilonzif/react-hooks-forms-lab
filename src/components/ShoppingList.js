8d1129dbddbdde28e63f8cc06a202be19dfae3e2
kilonzif <faithmueni6@gmail.com>
2022-09-26 10:36:30 +0300

    completed

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,26 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchItem, setSearchItem] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+
+  }).filter((item)=>{
+    return item.name.toLowerCase().includes(searchItem.toLowerCase());
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} search={searchItem} onSearchChange={setSearchItem}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
